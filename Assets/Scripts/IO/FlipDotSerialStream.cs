2781facd719b24a610d6d8945c3d3666d1afaa63
Hugo Yeung <34024260+hugoymh@users.noreply.github.com>
2020-04-14 17:19:02 +0800

    NuGet SerialPortStream WIP

---
+++ Assets/Scripts/IO/FlipDotSerialStream.cs
@@ -0,0 +1,17 @@
+﻿using RJCP.IO.Ports;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipDotSerialStream : MonoBehaviour
+{
+    public SerialPortStream sps;
+
+    private void Awake()
+    {
+        sps = new SerialPortStream("COM1", 57600, 8, Parity.None, StopBits.One);
+
+        sps.Open();
+        sps.
+    }
+}
